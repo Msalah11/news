3d007e2047cbdaf4f704bcb2b50d1ca45eede18d
MSalah <msalah0022@gmail.com>
2021-11-06 14:06:59 +0200

    Added relation between news and user

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,12 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class News extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
     protected $fillable = [
         'title', 'content', 'user_id'
     ];
+
+    /**
+     * A news post has one user.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
